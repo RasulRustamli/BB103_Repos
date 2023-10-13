e5da8038547923d00d69b1c32852a5e116b5b8b8
RasulRustamli <rasular@code.edu.az>
2023-10-13 11:44:56 +0400

    feat created bb103_weapon_task

---
+++ bb103_weapon_tasks/bb103_weapon_tasks/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace bb103_weapon_tasks
+{
+
+
+    class Program
+    {
+        static void Main()
+        {
+            Weapon weapon = new Weapon(40, 25);
+            bool check = true;
+            do
+            {
+                Console.WriteLine("-0 Informasiya almaq ucun");
+                Console.WriteLine("-1 Ates acmaq ucun");
+                Console.WriteLine("-2 Daragin dolmasi ucun lazim olan gulle miqdari");
+                Console.WriteLine("-3 Daragi deyismek");
+                Console.WriteLine("-4 Atis modunu deyismek");
+                Console.WriteLine("-5 Exit");
+                string input = Console.ReadLine();
+                switch (input)
+                {
+                    case "0":
+                        Console.WriteLine($"silah daraginin tutumu {weapon.MagazineCapacity} hal hazirki gulle sayi {weapon.Bullets} hal ahzirki ates modu {weapon.FireMode}");
+                        break;
+                    case "1":
+                        weapon.Shoot();
+                        break;
+                    case "2":
+                        weapon.GetRemainBulletCount();
+                        break;
+                    case "3":
+                        weapon.Reload();
+                        break;
+                    case "4":
+                        weapon.ChangeFireMode();
+                        break;
+                    case "5":
+                        check = false;
+                        break;
+                }
+
+            } while (check);
+
+
+        }
+    }
+}
---
+++ bb103_weapon_tasks/bb103_weapon_tasks/Weapon.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bb103_weapon_tasks
+{
+    public class Weapon
+    {
+        public int MagazineCapacity { get; set; }
+        public int Bullets { get; set; }
+        public string FireMode { get; set; }
+        public Weapon(int magazineCapacity, int bulletsInMagazine)
+        {
+            MagazineCapacity = magazineCapacity;
+            Bullets = bulletsInMagazine;
+            FireMode = "Single";
+        }
+
+
+        public void Shoot()
+        {
+            if(Bullets>0)
+            {
+                if (FireMode == "Single")
+                {
+                    Bullets--;
+                    Console.WriteLine("!Bang");
+                }
+                else
+                {
+                    //Bullets = 0;//bu sekildede olar
+                    while (Bullets > 0)
+                    {
+                        Bullets--;
+                        Console.WriteLine("!Bang");
+                    }
+
+                }
+            }
+            else
+            {
+                Console.WriteLine("Zehmet olmasa daragi doldurun");
+            }
+          
+        }
+        public void GetRemainBulletCount()
+        {
+            Console.WriteLine($"Daragin tam dolmasi ucun lazim olan gulle miqdari{MagazineCapacity-Bullets}");
+        }
+        public void Reload()
+        {
+            if(Bullets!=MagazineCapacity)
+            {
+                Bullets = MagazineCapacity;
+                Console.WriteLine("Darag tam doldu");
+            }
+            else
+            {
+                Console.WriteLine("Darag zaten doludur");
+            }
+           
+        }
+        public void ChangeFireMode()
+        {
+            if(FireMode=="Single")
+            {
+                FireMode = "Avtomatic";
+            }
+            else
+            {
+                FireMode = "Single";
+            }
+            Console.WriteLine($"Atis modu deyisdir hal hazirki mod {FireMode}");
+
+        }
+
+
+
+
+
+
+
+    }
+}
